85b2ffb95c918395c5c7c8bb16a0b55cade5e399
akashrawat2927 <akashrawat2927@gmail.com>
2023-04-23 04:55:34 +0530

    port changed to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const fileUpload = require('express-fileupload');
-const port =  80;
+const port =  3000;
 
 
 
