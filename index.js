0f7e10a877bc6fc2d7df6d4e18941a56f7862e20
akashrawat2927 <akashrawat2927@gmail.com>
2023-04-23 04:39:00 +0530

    port changed to 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const fileUpload = require('express-fileupload');
-const port = process.env.PORT || 8080;
+const port =  80;
 
 
 
