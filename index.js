aec51d6f5212bdb1355aad93705a7ade08a1155b
akashrawat2927 <akashrawat2927@gmail.com>
2023-04-23 04:20:34 +0530

    port changed to 3000

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const fileUpload = require('express-fileupload');
+const port = process.env.PORT || 3000
 
 
 
@@ -53,4 +54,4 @@ app.post('/upload', async(req, res) => {
 });
 
 // console.log(process.env.PORT);
-app.listen(process.env.PORT);
+app.listen(port,()=>console.log('listening on port ' +port));
